eef780346f789630ee2237450b5726e4b8947d2b
Anthony Periandri <56970077+AnthonyP26@users.noreply.github.com>
2022-02-27 12:14:43 -0500

    Update AJP_tiktactoe.py

--- AJP_tiktactoe.py
+++ AJP_tiktactoe.py
@@ -1,51 +1,75 @@
-tiles = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+tiles = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+tiles_str = ("1", "2", "3", "4", "5", "6", "7", "8", "9")
 
-divider = "-----"
-play_board = str(tiles[0]) + "|" + str(tiles[1]) + "|" + str(tiles[2])
-play_board_1 = str(tiles[3]) + "|" + str(tiles[4]) + "|" + str(tiles[5])
-play_board_2 = str(tiles[6]) + "|" + str(tiles[7]) + "|" + str(tiles[8])
+global turn_count
+turn_count = 0
+
 
 def board():
+  divider = "-----"
+  play_board = str(tiles[0]) + "|" + str(tiles[1]) + "|" + str(tiles[2])
+  play_board_1 = str(tiles[3]) + "|" + str(tiles[4]) + "|" + str(tiles[5])
+  play_board_2 = str(tiles[6]) + "|" + str(tiles[7]) + "|" + str(tiles[8])
+  
   print(play_board)
   print(divider)
   print(play_board_1)
   print(divider)
   print(play_board_2)
 
-board()
+while True:
 
-win = False
-while win == False:
-
-  def X_():
-    x_turn = int(input("X take your turn: "))
-    if isinstance(x_turn, int) == True:
-      if x_turn in range(0, 8):
-        board()
-        Y_()
-      else:
-        print("Not an option")
-        X_()
-    else:
-      print("Not an option")
-      X_()
-
-    tiles(range(0, 8)) == x_turn
+  def Turn_X():
     board()
-    
-  def Y_():
-    y_turn = int(input("Y take your turn: "))
-    if isinstance(y_turn, int) == True:
-      if y_turn in range(0, 8):
-        board(board())
-        X_()
+    x_turn = input("X take your turn: ")
+    if x_turn in tiles_str:
+      x_turn = int(x_turn)
+      if x_turn in tiles:
+        if isinstance(tiles[x_turn], int):
+          global turn_count
+          turn_count = turn_count+1
+          tiles[x_turn -1] = "X"
+          if turn_count < 9:
+            Turn_O()
+        else:
+          print("Not an option")
+          Turn_X()
       else:
         print("Not an option")
-        Y_()
+        Turn_X()
     else:
       print("Not an option")
-      Y_()
+      Turn_X()
 
+  def Turn_O():
+    board()
+    o_turn = input("O take your turn: ")
+    if o_turn in tiles_str:
+      o_turn = int(o_turn)
+      if o_turn in tiles:
+        if isinstance(tiles[o_turn], int):
+          global turn_count
+          turn_count = turn_count+1
+          tiles[o_turn -1] = "O"
+          print(turn_count)
+          Turn_X()
+        else:
+          print("Not an option")
+          Turn_O()
+      else:
+        print("Not an option")
+        Turn_O()
+    else:
+      print("Not an option")
+      Turn_O()
+  if turn_count >= 9:
+    board()
+    print("\nDraw")
+    break
     
+  #if tiles[0] == "X" and tiles[1] == "x" and tiles[2] =="X":
+  #  print("X wins")
+  #  break
   
-  X_()
+  Turn_X()
+  
