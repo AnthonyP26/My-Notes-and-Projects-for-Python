df3b668616882a7e35016514be365d9b711b1f2b
Anthony Periandri <56970077+AnthonyP26@users.noreply.github.com>
2022-09-15 09:00:17 -0400

    Update AJP_tiktactoe.py

--- AJP_tiktactoe.py
+++ AJP_tiktactoe.py
@@ -22,29 +22,39 @@ def turn(boardSet):
       if int(turn) in boardSet:
         boardSet[int(turn)] = player[i]
 
-        if boardSet[0] and boardSet[1] and boardSet[2] == player[i]:
+        if boardSet[0] == boardSet[1] == boardSet[2]:
+          board(boardSet)
           sys.exit(player[i] + "wins!")
 
-        if boardSet[3] and boardSet[4] and boardSet[5] == player[i]:
+        if boardSet[3] == boardSet[4] == boardSet[5]:
+          board(boardSet)
           sys.exit(player[i] + "wins!")
 
-        if boardSet[6] and boardSet[7] and boardSet[8] == player[i]:
+        if boardSet[6] == boardSet[7] == boardSet[8]:
+          board(boardSet)
           sys.exit(player[i] + "wins!")
 
-        if boardSet[0] and boardSet[3] and boardSet[6] == player[i]:
+        if boardSet[0] == boardSet[3] == boardSet[6]:
+          board(boardSet)
           sys.exit(player[i] + "wins!")
 
-        if boardSet[1] and boardSet[4] and boardSet[7] == player[i]:
+        if boardSet[1] == boardSet[4] == boardSet[7]:
+          board(boardSet)
           sys.exit(player[i] + "wins!")
 
-        if boardSet[2] and boardSet[5] and boardSet[8] == player[i]:
+        if boardSet[2] == boardSet[5] == boardSet[8]:
+          board(boardSet)
           sys.exit(player[i] + "wins!")
           
-        if boardSet[0] and boardSet[4] and boardSet[8] == player[i]:
+        if boardSet[0] == boardSet[4] == boardSet[8]:
+          board(boardSet)
           sys.exit(player[i] + "wins!")
           
-        if boardSet[2] and boardSet[4] and boardSet[6] == player[i]:
+        if boardSet[2] == boardSet[4] == boardSet[6]:
+          board(boardSet)
           sys.exit(player[i] + "wins!")
+
+          
 
         print("\n")
         break
